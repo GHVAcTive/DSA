ee871d110cd26b552e10add65d858f1e70ec0878
Master-Suyash <suyashkanhed3@gmail.com>
2025-01-21 17:06:02 +0530

    Bubble Sort Code Added

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class BubbleSort
+{
+    public static void main(String arg[])
+    {
+        Scanner r = new Scanner(System.in);
+        System.out.println("Enter a Size of Array : ");
+        int size = r.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter a Array Elements (Use space) : ");
+        for(int i=0; i<size; i++)
+        {
+            arr[i] = r.nextInt();
+        }
+        System.out.println("Sorted Implemented : ");
+        for(int k : BubbleSort(arr))
+        {
+            System.out.print(k+ " ");
+        }
+
+    }
+
+    public static int[] BubbleSort(int arr[])
+    {
+        for(int i=arr.length-1; i>=1 ;i--)
+        {
+            for(int j=0; j <=i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
