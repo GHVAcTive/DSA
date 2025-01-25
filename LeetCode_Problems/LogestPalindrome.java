df0bfc0eff0faba43e42ec719c48e92483592183
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-01-25 23:28:54 +0530

    Create LogestPalindrome.java

---
+++ LeetCode_Problems/LogestPalindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestPalindrome(String s)  {
+        if (s == null || s.length() < 1) return "";
+
+        String longest = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            // Odd-length palindrome
+            String oddPalindrome = expandAroundCenter(s, i, i);
+            // Even-length palindrome
+            String evenPalindrome = expandAroundCenter(s, i, i + 1);
+
+            // Update longest palindrome
+            if (oddPalindrome.length() > longest.length()) {
+                longest = oddPalindrome;
+            }
+            if (evenPalindrome.length() > longest.length()) {
+                longest = evenPalindrome;
+            }
+        }
+
+        return longest;
+    }
+
+    private String expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // Return the substring between left+1 and right
+        return s.substring(left + 1, right);
+    }
+}
