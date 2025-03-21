888742d4d05df10731f555851f286424189f2f2d
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-03-21 22:59:35 +0530

    Update 133_Q-R.java

--- LeetCode_Problems/133_Q-R.java
+++ LeetCode_Problems/133_Q-R.java
@@ -1,25 +1,5 @@
 import java.util.*;
 
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<>();
-    }
-    
-    public Node(int val) {
-        this.val = val;
-        neighbors = new ArrayList<>();
-    }
-    
-    public Node(int val, List<Node> neighbors) {
-        this.val = val;
-        this.neighbors = neighbors;
-    }
-}
-
 class Solution {
     private Map<Node, Node> map = new HashMap<>();
     
