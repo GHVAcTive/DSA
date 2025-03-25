6a26c62c0612a3064c322141511ba398450da090
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-03-25 22:23:33 +0530

    Create 46_Q-R.java

---
+++ LeetCode_Problems/46_Q-R.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));  // Valid permutation found
+            return;
+        }
+
+        for (int num : nums) {
+            if (current.contains(num)) {
+                continue; // Skip if the number is already used
+            }
+
+            current.add(num);
+            backtrack(nums, current, result);
+            current.remove(current.size() - 1); // Backtrack
+        }
+    }
+}
