4c75ab020352d1ea25a29a4a3cca0d430cc34bce
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-03-31 17:50:17 +0530

    Create 22_Q-R.java

---
+++ LeetCode_Problems/22_Q-R.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current, int open, int close, int max) {
+        // Base case: if the current string length equals 2 * n, add it to the result
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        // If the number of open parentheses is less than n, add an open parenthesis
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+
+        // If the number of closed parentheses is less than open, add a close parenthesis
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+
+    // Main method to test the function
+    public static void main(String[] args) {
+        GenerateParentheses gp = new GenerateParentheses();
+        int n = 3;
+        List<String> result = gp.generateParenthesis(n);
+        System.out.println(result);
+    }
+}
