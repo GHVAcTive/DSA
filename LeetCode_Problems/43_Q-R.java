66019383762d5ac4db9507bfffdf11b02b0ebc64
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-06 05:05:30 +0530

    Update 43_Q-R.java

--- LeetCode_Problems/43_Q-R.java
+++ LeetCode_Problems/43_Q-R.java
@@ -1,16 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
+class Solution {
+    public String multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
 
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>(); // value -> index
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + pos[p2];
+
+                pos[p2] = sum % 10;
+                pos[p1] += sum / 10;
             }
-            map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+
+        StringBuilder sb = new StringBuilder();
+        for (int p : pos) {
+            if (!(sb.length() == 0 && p == 0)) sb.append(p);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
