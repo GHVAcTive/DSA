bb7b9849eb65b92df3ee7e7c3eca0026acdf6d66
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-10 23:22:55 +0530

    Create 211_Q-R.java

---
+++ LeetCode_Problems/211_Q-R.java
@@ -0,0 +1,73 @@
+class WordDictionary {
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (!node.containsKey(ch)) {
+                node.put(ch, new TrieNode());
+            }
+            node = node.get(ch);
+        }
+        node.setEnd();
+    }
+
+    public boolean search(String word) {
+        return searchInNode(word, root);
+    }
+
+    private boolean searchInNode(String word, TrieNode node) {
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (ch == '.') {
+                for (char x = 'a'; x <= 'z'; x++) {
+                    if (node.containsKey(x) && searchInNode(word.substring(i + 1), node.get(x))) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (!node.containsKey(ch)) {
+                    return false;
+                }
+                node = node.get(ch);
+            }
+        }
+        return node.isEnd();
+    }
+
+    private class TrieNode {
+        private TrieNode[] links;
+        private final int R = 26;
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+}
