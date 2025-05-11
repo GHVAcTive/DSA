f90ce858c30d5473ccf742464c1ffddfcb2451c3
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-11 21:58:14 +0530

    Update 289_Q-R.java

--- LeetCode_Problems/289_Q-R.java
+++ LeetCode_Problems/289_Q-R.java
@@ -1,1 +1,25 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length, n = board[0].length;
+        int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+                        {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int lives = 0;
+                for (int[] dir : dirs) {
+                    int r = i + dir[0], c = j + dir[1];
+                    if (r >= 0 && r < m && c >= 0 && c < n)
+                        lives += board[r][c] & 1;
+                }
+                if ((board[i][j] & 1) == 1 && (lives == 2 || lives == 3))
+                    board[i][j] |= 2;
+                if ((board[i][j] & 1) == 0 && lives == 3)
+                    board[i][j] |= 2;
+            }
+        }
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] >>= 1;
+    }
+}
 
