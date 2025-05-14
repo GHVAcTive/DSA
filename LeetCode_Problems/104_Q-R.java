efc1153b895c3ab0d1f58425facade9c906221ca
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-14 23:30:06 +0530

    Create 104_Q-R.java

---
+++ LeetCode_Problems/104_Q-R.java
@@ -0,0 +1,8 @@
+
+// LeetCode Problem 104 - Maximum Depth of Binary Tree
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
