0ce1dffde070f1366269291952559762d445cc98
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-14 23:39:40 +0530

    Create 226.java

---
+++ LeetCode_Problems/226.java
@@ -0,0 +1,12 @@
+// LeetCode Problem 226 - Invert Binary Tree
+public class Solution226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+
