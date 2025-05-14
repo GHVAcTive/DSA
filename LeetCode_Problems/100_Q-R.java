cf986ef52e1e9cac4bf198eae4f0c9734a7fde44
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-14 23:29:18 +0530

    Create 100_Q-R.java

---
+++ LeetCode_Problems/100_Q-R.java
@@ -0,0 +1,9 @@
+
+// LeetCode Problem 100 - Same Tree
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
