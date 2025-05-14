43d531d120f0f3f4a7e27d759a8a8880a8a125ac
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-05-14 23:35:09 +0530

    Update 109.java

--- LeetCode_Problems/109.java
+++ LeetCode_Problems/109.java
@@ -1,19 +1,18 @@
-
-// LeetCode Problem 109 - Convert Sorted List to Binary Search Tree
 public class Solution {
-    public TreeNode sortedListToBST(ListNode head) {
-        return buildTree(head, null);
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
     }
-    private TreeNode buildTree(ListNode start, ListNode end) {
-        if (start == end) return null;
-        ListNode slow = start, fast = start;
-        while (fast != end && fast.next != end) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        TreeNode node = new TreeNode(slow.val);
-        node.left = buildTree(start, slow);
-        node.right = buildTree(slow.next, end);
+
+    private TreeNode buildTree(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildTree(nums, left, mid - 1);
+        node.right = buildTree(nums, mid + 1, right);
+
         return node;
     }
 }
+
+
