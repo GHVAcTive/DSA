571b2dc9690dc9c28d161487de246977b04f108e
Suyash <136662659+GHVAcTive@users.noreply.github.com>
2025-07-31 16:40:15 +0530

    Create 371.java

---
+++ LeetCode_Problems/371.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;     // carry bits
+            a = a ^ b;             // sum without carry
+            b = carry << 1;        // shift carry to left
+        }
+        return a;
+    }
+}
